7b088b5b3a7db101810fc0a0747ad015641adbbf
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-14 11:58:33 +0200

    Aggiornamento section review

--- src/components/ReviewsComponent.vue
+++ src/components/ReviewsComponent.vue
@@ -14,7 +14,15 @@
       <div class="row">
         <div class="col">
           <div class="container-review">
-            <img class="review_img" src="../assets/img/blog1-400x208.jpg" alt="" />
+            <div class="left left1"></div>
+            <div class="right right1">
+              <p class="p-grey">
+                "The Trainers at Avada Gym have helped me <br>reach my fitness goals beyand anything I could <br>of hoped for. I love it!"
+              </p>
+              <div>
+                <h4 class="p-grey"><span>-</span> Tara Smith</h4>
+              </div>
+            </div>
           </div>
         </div>
         <div class="col">
@@ -47,6 +55,13 @@ export default {
 
 
 <style lang="scss" scoped>
+.p-grey{
+  color:#6b6b6b;
+}
+
+.white{
+  color:white;
+}
 .reviews {
   height: 90vh;
   background-color: #6b6b6b;
@@ -72,17 +87,39 @@ export default {
   align-items: center;
   width: 400px;
   height: 200px;
-  background-color: red;
+  background-color: #252a2d;
   margin: 20px;
 
   .container-review {
     width: 573px;
     height: 161px;
-    background-color: blue;
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
     .review_img {
       width: 174px;
       height: 160px;
     }
   }
+
+  .left {
+    width: 30%;
+    height: 100%;
+    background-image: url("../assets/img/blog1.jpg");
+    background-size: 183%;
+  }
+
+  .right {
+    width: 70%;
+    height: 100%;
+    display:Flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    margin-left:50px;
+    line-height: 30px;
+    }
 }
 </style>
